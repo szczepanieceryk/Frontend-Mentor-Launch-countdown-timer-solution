352667e308d97af43844a2224305a1737e83ebd4
Eryk Szczepaniec <szczepanieceryk@gmial.com>
2021-11-02 17:03:15 +0100

    add README and some styles

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
+// assined HTML elements of a countdown timer
 const daysSpan = document.getElementById('days');
 const hoursSpan = document.getElementById('hours');
 const minutesSpan = document.getElementById('minutes');
 const secondsSpan = document.getElementById('seconds')
 
-// date of launch
-// const dayOfLaunch = new Date().toLocaleString().slice(0, 2);
+// always actual year,month,day data
+const actualYear = new Date().getFullYear()
+const actualMonth = new Date().toLocaleDateString().slice(2, 4)
+const actualDay = new Date().getDay();
 
-const daysToLaunch = 08;
-const hoursToLaunch = 23;
-const minutesToLaunch = 55;
-const secondsToLaunch = 41;
+const daysToLaunch = actualDay + 9;
 
-// const endTime = new Date(`2021,11,${daysToLaunch},${hoursToLaunch},${minutesToLaunch},${secondsToLaunch}`);
-const endTime = new Date('2021-11-07').getTime();
+//time of launch
+const endTime = new Date(`${actualYear}-${actualMonth}-${daysToLaunch}`).getTime();
 
-// time of launching
-// const time = endTime - nowTime;
-
+// interval function set to 1000 miliseconds (1 second) to update counter every second
 setInterval(() => {
-
+    //actual time
     const nowTime = new Date().getTime();
 
-    let days = Math.floor(endTime / (1000 * 60 * 60 * 24) - (nowTime / (1000 * 60 * 60 * 24)));
+    // calculate days from miliseconds (1000 miliseconds in second , 60 seconds in minute, 60 minutes in hour , 24 hours in day)
+    //then subtracting actual time from time of launch(end time) to get te time left
+    // Math.floor method to cut decimal numbers
 
+    let days = Math.floor(endTime / (1000 * 60 * 60 * 24) - (nowTime / (1000 * 60 * 60 * 24)));
+    // when the number of days/hours/minutes/seconds is less than 10, add "0" in front
     days = days < 10 ? `0${days}` : days;
 
     let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
@@ -37,6 +39,7 @@ setInterval(() => {
 
     seconds = seconds < 10 ? `0${seconds}` : seconds;
 
+    // assign calculated values to the right span elements
     daysSpan.textContent = days;
     hoursSpan.textContent = hours;
     minutesSpan.textContent = minutes;
